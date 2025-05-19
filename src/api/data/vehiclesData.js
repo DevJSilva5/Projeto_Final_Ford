39958fc0f7aaba012fa1c4267d71d68672a4ce3e
João Pedro Silva <joao2255.m9@gmail.com>
2025-05-19 10:34:10 -0300

    Update

--- src/api/data/vehiclesData.js
+++ src/api/data/vehiclesData.js
@@ -35,22 +35,17 @@ module.exports = {
        ],
        details: {
            "2FRHDUYS2Y63NHD22454": {
-               id: 1, odometro: 23344, nivelCombustivel: 76, status: "on", lat: -12.2322, long: -35.2314
+               id: 1, odometro: 23344, nivelCombustivel: "76%", status: "on", lat: -12.2322, long: -35.2314
            },
            "2RFAASDY54E4HDU34874": {
-               id: 2, odometro: 130000, nivelCombustivel: 19, status: "off", lat: -12.2322, long: -35.2314
+               id: 2, odometro: 130000, nivelCombustivel: "19%", status: "off", lat: -12.2322, long: -35.2314
            },
            "2FRHDUYS2Y63NHD22455": {
-               id: 3, odometro: 50000, nivelCombustivel: 90, status: "on", lat: -12.2322, long: -35.2314
+               id: 3, odometro: 50000, nivelCombustivel: "90%", status: "on", lat: -12.2322, long: -35.2314
            },
            "2RFAASDY54E4HDU34875": {
-               id: 4, odometro: 10000, nivelCombustivel: 25, status: "off", lat: -12.2322, long: -35.2314
+               id: 4, odometro: 10000, nivelCombustivel: "25%", status: "off", lat: -12.2322, long: -35.2314
            },
-           "2FRHDUYS2Y63NHD22654": {
-               id: 5, odometro: 23544, nivelCombustivel: 76, status: "on", lat: -12.2322, long: -35.2314
-           },
-           "2FRHDUYS2Y63NHD22854": {
-               id: 6, odometro: 23574, nivelCombustivel: 76, status: "on", lat: -12.2322, long: -35.2314
-           }
+
        }
    };
